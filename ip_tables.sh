d0c376268d5bed085573e231e05f704fbf20b934
Emre Tarakci <tarakci.software@gmail.com>
2023-07-10 17:07:28 +0300

    Add firewall rules and firewall test

--- ip_tables.sh
+++ ip_tables.sh
@@ -3,7 +3,7 @@ declare -a NameSpaceArray=("client1" "client2" "server" "firewall")
 declare -a FirewallConnection=("client1" "client2" "server")
 
 examine_namespaces() {
-	echo -e "\n=> Examine the network namespaces"
+	printf "\n=> Examine the network namespaces"
 	for i in "${NameSpaceArray[@]}"
 	do
 		echo "== The network configuration of $i:"
@@ -12,7 +12,7 @@ examine_namespaces() {
 }
 
 add_namespaces() {
-	echo -e "\n=> Adding the network namespaces..."
+	printf "\n=> Adding the network namespaces..."
 	for i in "${NameSpaceArray[@]}"
 	do
 		sudo ip netns add "$i"
@@ -20,7 +20,7 @@ add_namespaces() {
 }
 
 remove_namespaces() {
-	echo -e "\n=> Deleting the network namespaces..."
+	printf "\n=> Deleting the network namespaces..."
 	for i in "${NameSpaceArray[@]}"
 	do
 		sudo ip netns del "$i"
@@ -29,7 +29,7 @@ remove_namespaces() {
 
 
 set_veth_connections() {
-	echo -e "\n=> Setting veth connections..."
+	printf "\n=> Setting veth connections..."
 	for i in "${FirewallConnection[@]}"
 	do
 		sudo ip link add "veth-$i" type veth peer name veth-firewall
@@ -39,7 +39,7 @@ set_veth_connections() {
 }
 
 activate_interfaces() {
-	echo -e "\n=> Add ip addresses and activate interfaces..."
+	printf "\n=> Add ip addresses and activate interfaces..."
 	counter=2
 	sudo ip -n firewall link set lo up
 	# sudo ip netns exec firewall ip route add 192.0.2.128/26 via 192.0.2.130 dev veth-server
@@ -64,7 +64,7 @@ activate_interfaces() {
 }
 
 test_pings() {
-	echo -e "\n=> ####### Ping the hosts"
+	printf "\n=> ####### Ping the hosts"
 	counter=2
 	for i in "${FirewallConnection[@]}"
 	do
@@ -78,34 +78,57 @@ test_pings() {
 	done
 }
 
+setup_routes() {
+	# Add Default Gateway to clients
+	sudo ip netns exec client2 ip route add default via 192.0.2.67 dev veth-firewall
+	sudo ip netns exec client1 ip route add default via 192.0.2.3 dev veth-firewall
+
+	sudo ip netns exec firewall sysctl -w net.ipv4.ip_forward=1
+
+	## SETUP ROUTES
+	sudo ip netns exec firewall ip route add 192.0.2.0/26 via 0.0.0.0 dev veth-client1
+	sudo ip netns exec firewall ip route add 192.0.2.64/26 via 0.0.0.0 dev veth-client2
+	sudo ip netns exec firewall ip route add 192.0.2.128/26 via 0.0.0.0 dev veth-server
+
+	sudo ip netns exec server ip route add 192.0.2.0/26 via 192.0.2.131 dev veth-firewall
+	sudo ip netns exec server ip route add 192.0.2.64/26 via 192.0.2.131 dev veth-firewall
+}
+
+setup_firewall() {
+	# sudo ip netns exec firewall iptables -A INPUT -p icmp -i veth-client1 -j DROP
+	# sudo ip netns exec firewall iptables -A INPUT -i veth-client1 -j DROP
+	# sudo ip netns exec firewall iptables -A INPUT -i veth-client2 -j DROP
+	# sudo ip netns exec firewall iptables -A INPUT -i veth-server -j DROP
+	sudo ip netns exec firewall iptables -P POSTROUTING DROP
+}
+
+test_firewall() {
+	printf "\n############## TEST FIREWALL  #############"
+	printf "\n#### Client1 pings Server... (should ACCEPT) ==\n"
+	sudo ip netns exec client1 ping -c 1 192.0.2.130
+	printf "\n#### Client2 requests HTTP page from Server... (should ACCEPT) ==\n"
+	sudo ip netns exec client2 curl 192.0.2.130:8000
+	printf "\n#### Client2 pings Firewall... (should ACCEPT) ==\n"
+	sudo ip netns exec client2 ping -c 1 192.0.2.67
+	printf "\n#### Client1 pings Firewall... (should REJECT) ==\n"
+	sudo ip netns exec client1 ping -c 1 192.0.2.3
+	printf "\n############## TEST FIREWALL END ##########\n"
+}
+
 remove_namespaces
 add_namespaces
 set_veth_connections
 activate_interfaces
 examine_namespaces
-
-# Add Default Gateway to clients
-sudo ip netns exec client2 ip route add default via 192.0.2.67 dev veth-firewall
-sudo ip netns exec client1 ip route add default via 192.0.2.3 dev veth-firewall
-
-sudo ip netns exec firewall sysctl -w net.ipv4.ip_forward=1
-
-## SETUP ROUTES
-sudo ip netns exec firewall ip route add 192.0.2.0/26 via 0.0.0.0 dev veth-client1
-sudo ip netns exec firewall ip route add 192.0.2.64/26 via 0.0.0.0 dev veth-client2
-sudo ip netns exec firewall ip route add 192.0.2.128/26 via 0.0.0.0 dev veth-server
-
-sudo ip netns exec server ip route add 192.0.2.0/26 via 192.0.2.131 dev veth-firewall
-sudo ip netns exec server ip route add 192.0.2.0/26 via 192.0.2.131 dev veth-firewall
-
-# sudo ip netns exec server ip route add default via 192.0.2.131
-# sudo ip netns exec server ip route add 192.0.2.128/26 via 192.0.2.2
-
+setup_routes
 test_pings
-sudo ip netns exec server node server.js
+sudo ip netns exec server node server.js &
+sleep 0.5
+setup_firewall
+test_firewall
 # sudo ip netns exec firewall curl 192.0.2.130:8000
 # examine_namespaces
-
+printf ""
 
 
 
